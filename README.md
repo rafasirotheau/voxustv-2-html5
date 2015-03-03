9dceb439fbe5a1e3b871eab0c77455d692712369
Rafael Sirotheau <rsirotheau@gmail.com>
2015-03-03 17:07:46 -0300

    See description
    - Não é mais necessário jQuery na página para funcionar
    - @require adicionado para docReady()
    - Roda em todas as urls que usem o protocolo http

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # voxustv-2-html5
-VoxusTV-2-HTML5 é um userscript que substitui os players de vídeo do Voxus.TV pelo player padrão do HTML5 (Utilizando a tag ```<video>```).
+VoxusTV-2-HTML5 é um userscript que irá substituir/remover os players de vídeo do Voxus.TV pelo player padrão do HTML5 (Utilizando a tag ```<video>```).
 
 ## Versão
 
-0.9.0
+1.0.0
 
 ## Changelog
+
+### 1.0.0
+
+* Não é mais necessário jQuery na página para funcionar
+* @require adicionado para docReady()
+* Roda em todas as urls que usem o protocolo http
 
 ### 0.9.0
 
@@ -14,15 +20,17 @@ VoxusTV-2-HTML5 é um userscript que substitui os players de vídeo do Voxus.TV 
 
 ## Instalação
 
-Utilize plugins/extensões do seu navegador que permitam a instalação de userscripts.
+Instale complementos/plugins/extensões no seu navegador que permitam a instalação de userscripts. Algumas sugestões:
+* [Tampermonkey](http://tampermonkey.net/ "Tampermonkey")
+* [Greasemonkey](http://www.greasespot.net/ "Greasemonkey")
 
 ## Desenvolvimento
 
-Quer contribuir? Fique a vontade! Acredito que o foco agora deve ser procurar outros sites/blogs que utilizem este player para tentar tornar o script universal (**TODO**).
+Quer contribuir? Fique a vontade! De sugestões ou commits ao script.
 
 ## Todo's
 
-- 1.0.0 - Suporte universal
+- Refatorar código
 
 ## License
 
--- userscript.js
+++ userscript.js
@@ -1,137 +1,143 @@
 // ==UserScript==
 // @name         Voxus.TV to HTML5
 // @namespace    http://github.com/rafasirotheau/voxustv-2-html5
-// @version      0.9
+// @version      0.9.1
 // @description  Troca o Voxus.TV Player pelo HTML5
 // @author       Rafael Sirotheau
-// @match        http://www.naosalvo.com.br/*
+// @match        http://*/*
 // @grant        none
+// @require      https://raw.githubusercontent.com/jfriend00/docReady/master/docready.js
 // ==/UserScript==
 
-var scriptName = "Voxus.TV to HTML5",
-    debug = true,
-    version = 0.9;
-
-function debugThis(msg) {
-    if(!debug)
-        return false;
-    
-    console.log('['+scriptName+' v.'+version+'] '+msg);
+if(self!=top) {
+	// Não executar script em iframes.
+	return false;
 }
 
-/**
- * jQuery.ajax mid - CROSS DOMAIN AJAX 
- * ---
- * @author James Padolsey (http://james.padolsey.com)
- * @version 0.11
- * @updated 12-JAN-10
- * ---
- * Note: Read the README!
- * ---
- * @info http://james.padolsey.com/javascript/cross-domain-requests-with-jquery/
- */
+var scriptName = "Voxus.TV to HTML5",
+	debug = true,
+	version = "1.0.0";
 
-jQuery.ajax = (function(_ajax){
-    
-    var protocol = location.protocol,
-        hostname = location.hostname,
-        exRegex = RegExp(protocol + '//' + hostname),
-        YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
-        query = 'select * from html where url="{URL}" and xpath="*"';
-    
-    function isExternal(url) {
-        return !exRegex.test(url) && /:\/\//.test(url);
-    }
-    
-    return function(o) {
-        
-        var url = o.url;
-        
-        if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
-            
-            // Manipulate options so that JSONP-x request is made to YQL
-            
-            o.url = YQL;
-            o.dataType = 'json';
-            
-            o.data = {
-                q: query.replace(
-                    '{URL}',
-                    url + (o.data ?
-                        (/\?/.test(url) ? '&' : '?') + jQuery.param(o.data)
-                    : '')
-                ),
-                format: 'xml'
-            };
-            
-            // Since it's a JSONP request
-            // complete === success
-            if (!o.success && o.complete) {
-                o.success = o.complete;
-                delete o.complete;
-            }
-            
-            o.success = (function(_success){
-                return function(data) {
-                    
-                    if (_success) {
-                        // Fake XHR callback.
-                        _success.call(this, {
-                            responseText: (data.results[0] || '')
-                                // YQL screws with <script>s
-                                // Get rid of them
-                                .replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '')
-                        }, 'success');
-                    }
-                    
-                };
-            })(o.success);
-            
-        }
-        
-        return _ajax.apply(this, arguments);
-        
-    };
-    
-})(jQuery.ajax);
+function debugThis(msg) {
+	if(!debug)
+		return false;
 
+	console.log('['+scriptName+' v.'+version+'] '+msg);
+}
 
+/* Ajax Class */
+var AjaxRequest = function() {
+	this.url = null;
+	this.ifr = null;
+	this.xmlhttp = null;
 
-$(function() {
-    
-    debugThis("Iniciando script...");
-    var $iframes = $('iframe[data-original^="http://www.voxus.tv/player/view"]');
-    
-    debugThis("Encontrado(s) "+$iframes.length+" Voxus.TV Player(s)");
-    console.log($iframes);
-    
-    $iframes.each(function(i) {
-        debugThis("Tentando substituir o "+(i+1)+"º player...");
-        var src= $(this).data('original');
-        
-        console.log($(this).parent());
-        
-        $.ajax({
-            url: src,
-            type: 'GET',
-            success: function(res) {
-                console.log(res.responseText)
-                
-                var thumbMeta = $(res.responseText).filter('meta[content$="png"]').attr('content');
-                
-                var video_name = thumbMeta.split('videos/')[1];
-                    video_name = video_name.split('.')[0];
-                    video_name = video_name.split('_')[0];
-                    video_name = video_name.substr(1);
+	this.doRequest = function(url,ifr,count) {
+		debugThis(count+'º player: Iniciando requisição Ajax!');
 
-                var HTML5_SRC = "http://d37qvuz6ev41ur.cloudfront.net/v_"+video_name+".mp4";
-                var $PARENT = $iframes.filter(":eq("+i+")").parent();
-                $iframes.filter(":eq("+i+")").remove();
-                $PARENT.html('<video width="640" height="360" controls><source src="'+HTML5_SRC+'" type="video/mp4">Your browser does not support the video tag.</video>');
-                
-                debugThis("Player "+(i+1)+" substituído!");
-            }
-        });
-    })
-    
+		this.url = url;
+		this.ifr = ifr;
+		this.xmlhttp=GetXmlHttpObject();
+
+		// Verifica se a requisição foi criada sem erros.
+		if (this.xmlhttp==null) {
+			alert('Seu navegador não suporta AJAX!');
+			return false;
+		}
+		
+		var requisicao = this.xmlhttp;
+
+		requisicao.onreadystatechange=function() {
+			
+			if(typeof requisicao === 'undefined') {
+				return false;
+			}
+
+			if (requisicao.readyState==4 && requisicao.status==200) { 
+				
+				var response = requisicao.responseText.replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi, '');				
+
+				var thumbSrc = response.split('.png"');
+					thumbSrc = thumbSrc[0].split('content="');
+					thumbSrc = thumbSrc[thumbSrc.length-1] + '.png';
+				
+					
+				var video_name = thumbSrc.split('videos/')[1];
+					video_name = video_name.split('.')[0];
+					video_name = video_name.split('_')[0];
+					video_name = video_name.substr(1);
+				
+				debugThis(count+'º player: Resposta OK! Substituindo....');
+
+				var videoSrc = "http://d37qvuz6ev41ur.cloudfront.net/v_"+video_name+".mp4";
+				debugThis(count+'º player: Video SRC: '+videoSrc);
+				
+				ifr.parentNode.innerHTML = '<video width="640" height="360" controls><source src="'+videoSrc+'" type="video/mp4">Seu navegador não suporta videos HTML5.</video>';
+				
+				debugThis(count+"º player: Substituído com sucesso!");
+			}
+		};
+
+		requisicao.open("GET",url,true);
+		requisicao.send();
+	}
+}
+function GetXmlHttpObject() {
+	var xmlHttp=null;
+	try {
+		// Firefox, Opera 8.0+, Safari
+		xmlHttp=new XMLHttpRequest();
+	}
+	catch (e) {
+		// Internet Explorer
+		try {
+			xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+		}
+		catch (e) {
+			xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	}
+	return xmlHttp;
+}
+
+var avoid = [],
+	ajax_request= [];
+
+docReady(function() {
+	debugThis("Iniciando script...");
+
+	var iframes = document.querySelectorAll('iframe[data-original^="http://www.voxus.tv/player/view"], iframe[src^="http://www.voxus.tv/player/view"]');
+	if(iframes.length <= 0) {
+		debugThis("Nenhum player encontrado. Finalizando o script...");
+		return false;
+	}
+
+	// Remover elementos escondidos - NaoSalvo não é o que chamamos de responsivo. Sendo assim, existem dois elementos de iframe: um para mobile e outro para desktop
+	var found = 0;
+	for (var i = iframes.length-1; i >= 0; i--) {
+		if (iframes[i].offsetWidth > 0 && iframes[i].offsetHeight > 0) {
+			avoid[i] = false;
+			found++;
+		} else {
+			avoid[i] = true;
+			
+		}
+	}
+	
+	debugThis("Encontrado(s) "+found+" Voxus.TV Player(s)");
+	var count = 1;
+	for(i=0;i<iframes.length;i++) {
+		if(!avoid[i]) {
+			var curr_iframe = iframes[i];
+			debugThis(count+"º player: Tentando substituir...");
+
+			var src= curr_iframe.getAttribute('data-original') || curr_iframe.getAttribute('src');
+			var requestUrl = 'http://query.yahooapis.com/v1/public/yql?'+
+			'q='+encodeURIComponent('select * from html where url="'+src+'" and xpath="*"')+
+			'&format=xml';
+
+			ajax_request[i] = new AjaxRequest;
+			ajax_request[i].doRequest(requestUrl, curr_iframe, count)
+			count++;
+		}
+	}
 });
